07e5906f166a7448135b30a6fabd5a7e28ddf02a
John Salib <john.salib@nl.abnamro.com>
2023-11-05 10:08:59 +0100

    Display JSwing Dialog

--- src/main/kotlin/com/example/intellijplugin/GenerateMVIAction.kt
+++ src/main/kotlin/com/example/intellijplugin/GenerateMVIAction.kt
@@ -2,15 +2,16 @@ package com.example.intellijplugin
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
-import com.intellij.openapi.ui.Messages
 
 class GenerateMVIAction : DumbAwareAction("MVI") {
 
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
 
-        val dialogue = NameInput();
-        Messages.showDialog("Hello", "Title", emptyArray(), 0, Messages.getInformationIcon())
+        val dialog = NameInput();
+        dialog.pack()
+        dialog.isVisible = true
+//        Messages.showDialog("Hello", "Title", emptyArray(), 0, Messages.getInformationIcon())
 
     }
 
