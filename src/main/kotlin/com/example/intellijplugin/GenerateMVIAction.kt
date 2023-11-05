7a8d396b2086a9ff7a97d3450ad9a587e081b545
John Salib <john.salib@nl.abnamro.com>
2023-11-05 11:54:08 +0100

    Bind NameInputDialog to Form

--- src/main/kotlin/com/example/intellijplugin/GenerateMVIAction.kt
+++ src/main/kotlin/com/example/intellijplugin/GenerateMVIAction.kt
@@ -1,5 +1,7 @@
 package com.example.intellijplugin
 
+import com.example.intellijplugin.dialog.NameInputDialog
+import com.example.intellijplugin.dialog.NameInputDialogListener
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
 
@@ -8,9 +10,18 @@ class GenerateMVIAction : DumbAwareAction("MVI") {
     override fun actionPerformed(e: AnActionEvent) {
         val project = e.project ?: return
 
-        val dialog = NameInput();
-        dialog.pack()
-        dialog.isVisible = true
+//        val dialog = NameInput();
+//        dialog.pack()
+//        dialog.isVisible = true
+        NameInputDialog(project, object : NameInputDialogListener {
+            override fun onOk(name: String) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onCancel() {
+                TODO("Not yet implemented")
+            }
+        }).showAndGet()
 //        Messages.showDialog("Hello", "Title", emptyArray(), 0, Messages.getInformationIcon())
 
     }
--- src/main/kotlin/com/example/intellijplugin/dialog/NameInputDialog.kt
+++ src/main/kotlin/com/example/intellijplugin/dialog/NameInputDialog.kt
@@ -3,11 +3,16 @@ package com.example.intellijplugin.dialog
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.ui.DialogWrapper
 import javax.swing.JComponent
+import javax.swing.JPanel
+import javax.swing.JTextField
 
 class NameInputDialog(
     private val project: Project,
     private val listener: NameInputDialogListener,
 ) : DialogWrapper(project) {
+
+    var mviNameField: JTextField? = null
+    var contentPanel: JPanel? = null
 
     init {
         init()
@@ -15,7 +20,7 @@ class NameInputDialog(
     }
 
     override fun createCenterPanel(): JComponent? {
-        TODO("Not yet implemented")
+        return contentPanel
     }
 
 }
