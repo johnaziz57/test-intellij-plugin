d49670584c1c57af486c06b03d9a56ff2f5138f9
John Salib <john.salib@nl.abnamro.com>
2023-11-08 13:52:01 +0100

    Trying to get gradle to work

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,6 +8,7 @@ group = "com.example"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    maven { url = uri("https://repo.gradle.org/gradle/libs-releases") }
     mavenCentral()
 }
 
@@ -20,7 +21,7 @@ dependencies {
 // Configure Gradle IntelliJ Plugin
 // Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
 intellij {
-    version.set("2021.3.3")
+    version.set("2022.2.1")
     type.set("IC") // Target IDE Platform
 
     plugins.set(listOf(/* Plugin Dependencies */))
@@ -29,11 +30,11 @@ intellij {
 tasks {
     // Set the JVM compatibility versions
     withType<JavaCompile> {
-        sourceCompatibility = "11"
-        targetCompatibility = "11"
+        sourceCompatibility = "17"
+        targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-        kotlinOptions.jvmTarget = "11"
+        kotlinOptions.jvmTarget = "17"
     }
 
     patchPluginXml {
--- src/main/kotlin/com/example/intellijplugin/actions/gradle/GradleAction.kt
+++ src/main/kotlin/com/example/intellijplugin/actions/gradle/GradleAction.kt
@@ -2,51 +2,52 @@ package com.example.intellijplugin.actions.gradle
 
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.project.DumbAwareAction
-import org.gradle.tooling.GradleConnectionException
-import org.gradle.tooling.GradleConnector
-import org.gradle.tooling.ResultHandler
-import java.io.File
 import java.io.IOException
 import java.net.HttpURLConnection
 import java.net.URL
 
 class GradleAction : DumbAwareAction("Gradle Action") {
     override fun actionPerformed(e: AnActionEvent) {
-        test()
-        val projectPath = e.project?.basePath ?: return
-        val connector = GradleConnector.newConnector()
-        connector.forProjectDirectory(File(projectPath))
-        val connection = connector
-            .useBuildDistribution()
-            .connect()
-        try {
-            val resultHandler = object : ResultHandler<Void> {
-                override fun onComplete(result: Void?) {
-                    println("complete")
-                    println(result)
-                }
+//        test()
+//        val projectPath = e.project?.basePath ?: return
+//        val connector = GradleConnector.newConnector()
+//        connector.forProjectDirectory(File(projectPath))
 
-                override fun onFailure(failure: GradleConnectionException?) {
-                    println("failure: ${failure?.localizedMessage}")
-                }
-
-            }
-            connection.newBuild()
-                .forTasks("iDD")
-                .setStandardOutput(System.out)
-                .setJavaHome(File("/Users/john/Library/Java/JavaVirtualMachines/corretto-11.0.21/Contents/Home"))
-                .run(resultHandler)
-
-
-        } catch (e: Exception) {
-            println("SOMETHING WRONG")
-        } finally {
-            connection.close()
-        }
+//        application.executeOnPooledThread {
+//            val connection = connector
+//                .useBuildDistribution()
+//                .connect()
+//            try {
+//                val gradleProject = connection.getModel(GradleProject::class.java)
+//                gradleProject.tasks.forEach{ println(it) }
+////            val resultHandler = object : ResultHandler<Void> {
+////                override fun onComplete(result: Void?) {
+////                    println("complete")
+////                    println(result)
+////                }
+////
+////                override fun onFailure(failure: GradleConnectionException?) {
+////                    println("failure: ${failure?.localizedMessage}")
+////                }
+////
+////            }
+////            connection.newBuild()
+////                .forTasks("iDD")
+////                .setStandardOutput(System.out)
+////                .setJavaHome(File("/Users/john/Library/Java/JavaVirtualMachines/corretto-11.0.21/Contents/Home"))
+////                .run(resultHandler)
+//
+//
+//            } catch (e: Exception) {
+//                println("SOMETHING WRONG: ${e.localizedMessage}")
+//            } finally {
+//                connection.close()
+//            }
+//        }
     }
 
 
-    private fun test() {
+    private fun testInternetConnection() {
         try {
             val url = URL("https://www.google.com")
             val urlConn = url.openConnection() as HttpURLConnection
